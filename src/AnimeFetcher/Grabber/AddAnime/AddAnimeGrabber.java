30e2e6e67bc8f85d9ab30db8a2a5710c519931e4
kareem2048 <karim.salah2048@gmail.com>
2017-03-30 01:23:55 +0200

    fix errors on running the project and add implementations for the abstracted methodes

--- src/AnimeFetcher/Grabber/AddAnime/AddAnimeGrabber.java
+++ src/AnimeFetcher/Grabber/AddAnime/AddAnimeGrabber.java
@@ -6,4 +6,13 @@ import AnimeFetcher.Grabber.Grabber;
  * Created by kareem on 30/03/17.
  */
 public class AddAnimeGrabber extends Grabber {
+    @Override
+    protected void enQueueAnimeLink(String link) {
+        
+    }
+
+    @Override
+    protected void analyze(String websiteData) {
+
+    }
 }
