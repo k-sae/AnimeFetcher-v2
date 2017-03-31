810903ef52ac58a4b9daf053f206a5ee873973f4
kareem2048 <karim.salah2048@gmail.com>
2017-03-31 02:00:19 +0200

    add more functionallity grabber is now capable of downloading web pages remove useless functions

--- src/AnimeFetcher/Grabber/AddAnime/AddAnimeGrabber.java
+++ src/AnimeFetcher/Grabber/AddAnime/AddAnimeGrabber.java
@@ -2,10 +2,18 @@ package AnimeFetcher.Grabber.AddAnime;
 
 import AnimeFetcher.Grabber.Grabber;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
 /**
  * Created by kareem on 30/03/17.
  */
 public class AddAnimeGrabber extends Grabber {
+    public AddAnimeGrabber(ArrayList<String> animeLink) {
+        super(animeLink);
+    }
+
     @Override
     protected void enQueueAnimeLink(String link) {
         
@@ -14,5 +22,15 @@ public class AddAnimeGrabber extends Grabber {
     @Override
     protected void analyze(String websiteData) {
 
+    }
+
+    @Override
+    protected URL fetchUrl(String animeLink) {
+        try {
+            return new URL(animeLink);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- src/AnimeFetcher/Main.java
+++ src/AnimeFetcher/Main.java
@@ -1,5 +1,6 @@
 package AnimeFetcher;
 
+import AnimeFetcher.Grabber.AddAnime.AddAnimeGrabber;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,6 +15,8 @@ public class Main extends Application {
         primaryStage.setTitle("Anime Fetcher");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        //TODO
+        // Saver to test the grabber
     }
 
 
