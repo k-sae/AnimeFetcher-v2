7b71c060da4ff65661d4551b0e0d7fd63c042ce8
kareem <karim.salah2048@gmail.com>
2017-06-01 23:45:21 +0200

    checkpoint to build the ui from scratch special thanks to the shitty shit javafx :)

--- src/AnimeFetcher/View/AddAnimeViewer.java
+++ src/AnimeFetcher/View/AddAnimeViewer.java
@@ -1,21 +1,32 @@
 package AnimeFetcher.View;
 
-import AnimeFetcher.Grabber.AddAnime.AddAnimeGrabber;
 import AnimeFetcher.Model.AddAnimeAnime;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.SingleSelectionModel;
-import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
 /**
  * Created by kemo on 31/05/2017.
  */
-public class AddAnimeViewer extends GridPane {
+
+/** UI VISUAL TREE
+ *                  GridPane    | row1 -> BorderPane |  left    -> HBox     | -> ComboBox anime_list
+ *                                                                          | -> NumericTextFiend startEp
+ *                                                                          | -> NumericTextFiend endEp
+ *
+ *                                                   |  Right   -> HBox     | -> Button Download_btn
+ *                              | row2 ->
+ *
+ */
+public class AddAnimeViewer extends GridPane implements EventHandler<MouseEvent> {
     public AddAnimeViewer() {
         setConstrains();
         initializePrompts();
@@ -23,6 +34,7 @@ public class AddAnimeViewer extends GridPane {
     private ComboBox<AddAnimeAnime> animeList;
     private NumericTextField startEp;
     private NumericTextField endEp;
+    private final int MARGIN_VALUE = 5;
     private void setConstrains() {
         final int SIZE = 4;
         for (int i = 0; i < SIZE ; i++) {
@@ -39,13 +51,14 @@ public class AddAnimeViewer extends GridPane {
     }
     private void initializePrompts()
     {
-        final int NO_OF_ITEMS  = 4;
-        GridPane gridPane = new GridPane();
-        for (int i = 0; i < NO_OF_ITEMS; i++) {
-            ColumnConstraints columnConstraints = new ColumnConstraints();
-            columnConstraints.setPercentWidth(100 / NO_OF_ITEMS);
-            gridPane.getColumnConstraints().add(columnConstraints);
-        }
+//        final int NO_OF_ITEMS  = 4;
+//        GridPane gridPane = new GridPane();
+//        for (int i = 0; i < NO_OF_ITEMS; i++) {
+//            ColumnConstraints columnConstraints = new ColumnConstraints();
+//            columnConstraints.setPercentWidth(100 / NO_OF_ITEMS);
+//            gridPane.getColumnConstraints().add(columnConstraints);
+//        }
+
         animeList = new ComboBox<>();
         animeList.getItems().add(new AddAnimeAnime("one piece", "123"));
         animeList.getItems().add(new AddAnimeAnime("one", "123"));
@@ -74,20 +87,37 @@ public class AddAnimeViewer extends GridPane {
                 animeList.getEditor().setText(newValue.replaceAll("[^\\d.]", ""));
             }
         });
+
         animeList.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println(animeList.getSelectionModel().getSelectedItem());
             }
         });
-        gridPane.add(animeList,0,0);
-        add(gridPane,0,0);
-        startEp = new NumericTextField();
-        startEp.setMaxWidth(50);
-        endEp = new NumericTextField();
-        endEp.setMaxWidth(50);
-        gridPane.add(startEp,1,0);
-        gridPane.add(endEp,2,0);
+        startEp = initNumericTextField();
+        endEp = initNumericTextField();
+        HBox lHBox = new HBox();
+        lHBox.getChildren().addAll(animeList,startEp,endEp);
+        BorderPane borderPane = new BorderPane();
+        borderPane.setLeft(lHBox);
+        GridPane.setMargin(borderPane, new Insets(MARGIN_VALUE));
+        add(borderPane,0,0);
+//        gridPane.add(animeList,0,0);
+//        add(gridPane,0,0);
+//        gridPane.add(startEp,1,0);
+//        gridPane.add(endEp,2,0);
+    }
+    private NumericTextField initNumericTextField()
+    {
+        NumericTextField child = new NumericTextField();
+        child.setPrefWidth(50);
+        HBox.setMargin(child,new Insets(0,0,0,10));
+        return child;
     }
 
+    @Override
+    public void handle(MouseEvent event) {
+        //TODO
+        System.out.println(":)");
+    }
 }
