8b1928543b784bb674fb5ad1c579ecb7d1dc8db3
kareem2048 <karim.salah2048@gmail.com>
2017-06-21 01:15:59 +0200

    update downloader class to support cookies

--- src/AnimeFetcher/Grabber/Downloader/Downloader.java
+++ src/AnimeFetcher/Grabber/Downloader/Downloader.java
@@ -15,26 +15,28 @@ public class Downloader {
     private ArrayList<DownloadProgressListener> downloadProgressListeners;
     private String location;
     private String fileName;
+    private ArrayList<String> params;
+    private String downloader;
+    private final String DEFAULT_CACHE_LOCATION = "cache/";
     public Downloader() {
         downloadProgressListeners = new ArrayList<>();
         location = "downloads";
         fileName = "video.mp4";
+        params = new ArrayList<>();
+        downloader = getBasicDownloader();
+        params.add(downloader);
     }
     public void startDownloading(String url)
     {
         FileManager.getInstance().createFolder(location);
         try {
             // list with all params ti start wget
-            ArrayList<String> params = new ArrayList<>(4);
-            params.add(getBasicDownloader());
             params.add("-O");
             params.add(location + "/" + fileName);
             params.add("-c");
             params.add(url);
             triggerOnStart();
-            ProcessBuilder processBuilder = new ProcessBuilder(params);
-            processBuilder.redirectErrorStream(true); // for some reason it will not work without it :0
-            Process process = processBuilder.start();
+            Process process = startDownloader();
             BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
             //start listening for wget
@@ -46,6 +48,7 @@ public class Downloader {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        clearParams();
        triggerOnFinish();
     }
     private Progress parseProgress(String s)
@@ -119,5 +122,43 @@ public class Downloader {
         String s = System.getProperty("os.name").toLowerCase();
         if (s.contains("lin")) return "wget";
         else return "Wget/wget.exe";
+    }
+    public void cacheCookies(String url)
+    {
+        cacheCookies(url, DEFAULT_CACHE_LOCATION);
+    }
+    public void cacheCookies(String url, String location)
+    {
+        params.add("--save-cookies");
+        params.add(location + "/cookies");
+        params.add("--keep-session-cookies");
+        params.add("--delete-after");
+        params.add(url);
+        try {
+            startDownloader().waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        clearParams();
+    }
+    private Process startDownloader() throws IOException {
+        ProcessBuilder processBuilder = new ProcessBuilder(params);
+        processBuilder.redirectErrorStream(true); // for some reason it will not work without it :0
+        return processBuilder.start();
+    }
+    private void clearParams()
+    {
+        params.clear();
+        params.add(downloader);
+    }
+    public void downloadWithCookies(String url)
+    {
+        downloadWithCookies(url,DEFAULT_CACHE_LOCATION);
+    }
+    public void downloadWithCookies(String url,String cookiesLocation)
+    {
+        params.add("--load-cookies");
+        params.add(cookiesLocation);
+        startDownloading(url);
     }
 }
